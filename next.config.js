7731e33a2e00ff5e521c9efdda9a46bbf8be564c
Rudolf Pailer <rudolf.pailer@at.ibm.com>
2023-12-03 17:44:34 +0100

    IKS

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  rewrites: async () => {
+    return [
+      {
+        source: '/health',
+        destination: '/api/health',
+      },
+    ];
+  }
 }
 
 module.exports = nextConfig
